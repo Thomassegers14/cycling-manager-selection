8c09513b566d75e05dfe48c063113b7f8473f482
Thomassegers14 <segers.thomas@telenet.be>
2023-07-28 17:13:13 +0200

    add selection bars

--- y/src/main.ts
+++ y/src/main.ts
@@ -1,12 +1,16 @@
 import Papa from 'papaparse';
+import * as d3 from 'd3';
 
 new Vue({
   el: '#app',
   data: {
+    pointsMaxValue: 10,
+    ridersMaxValue: 12,
     csvData: [], // Hier worden de gegevens uit de CSV opgeslagen
     availableNames: [], // Array voor alleen de namen
     selectedNames: [],
     groupedData: [], // Array voor het gegroepeerde teamdata
+    margin: { top: 12, right: 6, bottom: 0, left: 90 }
   },
   methods: {
     async fetchCSVData() {
@@ -33,26 +37,182 @@ new Vue({
       }
     },
     submitSelection() {
-        if (this.selectedNames.length !== 10) {
-            alert('Selecteer exact 10 namen voordat je ze verstuurt.');
-            return;
-          }
+      if (this.selectedNames.length !== this.ridersMaxValue) {
+        alert('Selecteer exact 12 namen voordat je ze verstuurt.');
+        return;
+      }
     },
     resetSelection() {
-        this.selectedNames = [];
+      this.selectedNames = [];
     },
     getPointsClass(points) {
-        if (points == 4) {
-          return 'riderElement riderElement--highPoints';
-        } else if (points == 2) {
-          return 'riderElement riderElement--mediumPoints';
-        } else {
-          return 'riderElement riderElement--lowPoints';
-        }
-      },
+      if (points == 4) {
+        return 'riderElement riderElement--highPoints';
+      } else if (points == 2) {
+        return 'riderElement riderElement--mediumPoints';
+      } else {
+        return 'riderElement riderElement--lowPoints';
+      }
+    },
+    drawInitialRidersBarChart() {
+      const { width, height } = d3.select('.barChart--riders').node().getBoundingClientRect()
+
+      // set inner dimensions
+      const innerWidth = width - this.margin.left - this.margin.right
+      const innerHeight = height - this.margin.top - this.margin.bottom
+
+      // append the svg object to the body of the page
+      const svg = d3.select('.barChart--riders')
+        .append('svg')
+        .attr('width', width)
+        .attr('height', height)
+        .append('g')
+        .attr('transform', `translate(${this.margin.left}, ${this.margin.top})`)
+
+      const xScale = d3.scaleLinear().range([0, innerWidth]).domain([0, this.ridersMaxValue * 1.25])
+      const yScale = d3.scaleBand().range([innerHeight, 0]).domain(['Selectie']).padding(0.1)
+
+      svg
+        .append('rect')
+        .attr('class', 'bgRect')
+        .attr('y', yScale('Selectie'))
+        .attr('x', xScale(0))
+        .attr('height', yScale.bandwidth())
+        .attr('width', xScale(this.ridersMaxValue * 1.25))
+
+      svg
+        .append('rect')
+        .attr('class', 'bar')
+        .attr('y', yScale('Selectie'))
+        .attr('x', xScale(this.selectedNames.length))
+        .attr('height', yScale.bandwidth())
+        .attr('width', xScale(this.selectedNames.length))
+
+      svg.append('g')
+        .attr('class', 'axis axis__x--minor')
+        .call(
+          d3.axisTop(xScale)
+            .ticks(this.ridersMaxValue * 1.25)
+            .tickSize(-innerHeight)
+        )
+
+      svg.append('g')
+        .attr('class', 'axis axis__x')
+        .call(
+          d3.axisTop(xScale)
+            .tickValues([0, this.ridersMaxValue])
+            .tickSize(-innerHeight)
+        )
+
+      svg.append('g')
+        .attr('class', 'axis axis__y')
+        .call(
+          d3.axisLeft(yScale)
+          .tickSize(0)
+          .tickPadding(24)
+        )
+
+      svg.selectAll('.axis').select('.domain').remove()
+    },
+    drawInitialPointsBarChart() {
+      const { width, height } = d3.select('.barChart--points').node().getBoundingClientRect()
+
+      // set inner dimensions
+      const innerWidth = width - this.margin.left - this.margin.right
+      const innerHeight = height - this.margin.top - this.margin.bottom
+
+      // append the svg object to the body of the page
+      const svg = d3.select('.barChart--points')
+        .append('svg')
+        .attr('width', width)
+        .attr('height', height)
+        .append('g')
+        .attr('transform', `translate(${this.margin.left}, ${this.margin.top})`)
+
+      const totalSelectedPoints = this.selectedNames.reduce((total, name) => {
+        const rider = this.csvData.find(item => item.rider_name === name);
+        return total + Number(rider.points)
+      }, 0)
+
+      const xScale = d3.scaleLinear().range([0, innerWidth]).domain([0, this.pointsMaxValue * 1.25])
+      const yScale = d3.scaleBand().range([innerHeight, 0]).domain(['Punten']).padding(0.1)
+
+      svg
+        .append('rect')
+        .attr('class', 'bgRect')
+        .attr('y', yScale('Punten'))
+        .attr('x', xScale(0))
+        .attr('height', yScale.bandwidth())
+        .attr('width', xScale(this.pointsMaxValue * 1.25))
+
+      svg
+        .append('rect')
+        .attr('class', 'bar')
+        .attr('y', yScale('Punten'))
+        .attr('x', xScale(totalSelectedPoints))
+        .attr('height', yScale.bandwidth())
+        .attr('width', xScale(totalSelectedPoints))
+
+        svg.append('g')
+        .attr('class', 'axis axis__x--minor')
+        .call(
+          d3.axisTop(xScale)
+            .ticks(this.pointsMaxValue * 1.25 / 2)
+            .tickSize(-innerHeight)
+        )
+
+        svg.append('g')
+        .attr('class', 'axis axis__x')
+        .call(
+          d3.axisTop(xScale)
+            .tickValues([0, this.pointsMaxValue])
+            .tickSize(-innerHeight)
+        )
+
+      svg.append('g')
+        .attr('class', 'axis axis__y')
+        .call(
+          d3.axisLeft(yScale)
+          .tickSize(0)
+          .tickPadding(24)
+        )
+
+      svg.selectAll('.axis').select('.domain').remove()
+    },
+    drawPointsBarChart() {
+      const totalSelectedPoints = this.selectedNames.reduce((total, name) => {
+        const rider = this.csvData.find(item => item.rider_name === name);
+        return total + Number(rider.points);
+      }, 0);
+
+      const { width, height } = d3.select('.barChart--points').node().getBoundingClientRect()
+
+      // set inner dimensions
+      const innerWidth = width - this.margin.left - this.margin.right
+
+      // append the svg object to the body of the page
+      const svg = d3.select('.barChart--points')
+
+      const xScale = d3.scaleLinear().range([0, innerWidth]).domain([0, this.pointsMaxValue * 1.25])
+
+      svg.select('.bar').attr('width', xScale(totalSelectedPoints));
+    },
+    drawRidersBarChart() {
+      const { width } = d3.select('.barChart--riders').node().getBoundingClientRect()
+
+      // set inner dimensions
+      const innerWidth = width - this.margin.left - this.margin.right
+
+      // append the svg object to the body of the page
+      const svg = d3.select('.barChart--riders')
+
+      const xScale = d3.scaleLinear().range([0, innerWidth]).domain([0, this.ridersMaxValue * 1.25])
+
+      svg.select('.bar').attr('width', xScale(this.selectedNames.length));
+    },
   },
   watch: {
-    selectedNames: function(newNames) {
+    selectedNames: function (newNames) {
       const totalPoints = newNames.reduce((total, name) => {
         const selectedData = this.csvData.find(item => item.rider_name === name);
         return total + (selectedData ? Number(selectedData.points) : 0);
@@ -60,29 +220,34 @@ new Vue({
 
       console.log('Geselecteerde namen:', newNames);
       console.log('Totaalpunten:', totalPoints);
+
+      this.drawPointsBarChart()
+      this.drawRidersBarChart()
     },
   },
   computed: {
     totalPoints() {
-        return this.selectedNames.reduce((total, name) => {
-            const selectedData = this.csvData.find(item => item.rider_name === name);
-            return total + (selectedData ? Number(selectedData.points) : 0);
-        },0);
+      return this.selectedNames.reduce((total, name) => {
+        const selectedData = this.csvData.find(item => item.rider_name === name);
+        return total + (selectedData ? Number(selectedData.points) : 0);
+      }, 0)
     },
     isSubmitDisabled() {
-        return this.totalPoints > 6 || this.selectedNames.length !== 10;
+      return this.totalPoints > this.pointsMaxValue || this.selectedNames.length !== this.ridersMaxValue;
     },
     buttonMessage() {
-        if (this.totalPoints > 6) {
-            return "Teveel punten geselecteerd";
-        } else if (this.selectedNames.length !== 10) {
-            return "Niet genoeg renners geselecteerd";
-        } else {
-            return "Verstuur selectie";
-        }
+      if (this.totalPoints > 6) {
+        return "Teveel punten geselecteerd";
+      } else if (this.selectedNames.length !== this.ridersMaxValue) {
+        return "Niet genoeg renners geselecteerd";
+      } else {
+        return "Verstuur selectie";
+      }
     },
   },
   mounted() {
-    this.fetchCSVData(); // Haal de CSV-gegevens op bij het laden van de pagina
+    this.fetchCSVData()
+    this.drawInitialPointsBarChart()
+    this.drawInitialRidersBarChart()
   },
 });
